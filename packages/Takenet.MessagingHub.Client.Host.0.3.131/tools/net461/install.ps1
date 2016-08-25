35dce5277f9d4c42625e65d6a704408ae226caef
Luiz Guilherme Correa Louro <luizg@takenet.com.br>
2016-08-25 18:07:21 -0300

    add project to repository

---
+++ packages/Takenet.MessagingHub.Client.Host.0.3.131/tools/net461/install.ps1
@@ -0,0 +1,21 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+write-host "Trying to update mhh.exe project reference (if one exists)"
+
+$hostFile = "mhh.exe"
+$hostPath = Join-Path (Join-Path $installPath "tools\net461\") $hostFile
+
+$projectUri = [uri]$project.FullName;
+$hosttUri = [uri]$hostPath;
+$hostRelativePath = $projectUri.MakeRelative($hosttUri) -replace "/","\"
+
+$project.ProjectItems | ForEach-Object {
+	if ($_.FileNames(0).EndsWith($hostFile)) { 
+		$_.Remove()
+		$project.ProjectItems.AddFromFile($hostPath) | out-null
+		$project.ProjectItems.Item($hostFile).Properties.Item("CopyToOutputDirectory").Value = 2
+		$project.Save()
+		write-host "Updated reference to $hostPath"
+		continue
+	}
+}
