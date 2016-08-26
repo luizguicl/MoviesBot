ec099db0ca4bad61d97bce72b90559640a23b5c8
Luiz Guilherme Correa Louro <luizg@takenet.com.br>
2016-08-26 11:13:08 -0300

    reset project

--- MoviesBot/Class1.cs
+++ MoviesBot/Class1.cs
@@ -6,21 +6,7 @@ using System.Threading.Tasks;
 
 namespace MoviesBot
 {
-    public class MovieSearch
+    public class Class1
     {
-        private AppSettings _appSettings;
-        private ApiClient _apiClient;
-
-        public MovieSearch(AppSettings appSettings)
-        {
-            _appSettings = appSettings;
-            _apiClient = new ApiClient(_appSettings);
-        }
-
-        public void GetPerson(string name)
-        {
-            _apiClient.GetPerson("kevin bacon");
-
-        }
     }
 }
